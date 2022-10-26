4f77b6a3968d00c83be2ff0d2b37daa91bf9f9c7
Luis <lerhardt@rhrk.uni-kl.de>
2022-10-27 01:03:17 +0200

    footer update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,20 +4,35 @@ interface IFooterProps {}
 
 const Footer: React.FunctionComponent<IFooterProps> = (props) => {
   return (
-    <div className="absolute bottom-0 px-20 w-full">
+    <div className="px-5 sm:px-20 w-full">
       <div
-        className="w-full h-16 flex justify-center items-center
+        className="w-full h-16 py-2
    border-t border-slate-600 dark:border-slate-500 text-slate-600 dark:text-slate-400"
       >
-        Made by Luis Erhardt
-        <a href="https://github.com/LuisErhardt/translate" className="mx-5" target="_blank" rel="noopener noreferrer">
-          <img className="dark:hidden h-8" src={process.env.PUBLIC_URL + "/GitHub-Mark-64px.png"} alt="" />
-          <img className="hidden dark:block h-8" src={process.env.PUBLIC_URL + "/GitHub-Mark-Light-64px.png"} alt="" />
-        </a>
-        Using
-        <a href="https://github.com/LibreTranslate/LibreTranslate">LibreTranslate API</a>
-        Hosted by
-        <a href="https://github.com/argosopentech/argos-translate/">Argos Translate</a>
+        <p className="flex justify-center">
+          Made by Luis Erhardt &nbsp;
+          <a href="https://github.com/LuisErhardt/translate" className="" target="_blank" rel="noopener noreferrer">
+            <img className="dark:hidden h-8" src={process.env.PUBLIC_URL + "/GitHub-Mark-64px.png"} alt="" />
+            <img
+              className="hidden dark:block h-8"
+              src={process.env.PUBLIC_URL + "/GitHub-Mark-Light-64px.png"}
+              alt=""
+            />
+          </a>
+        </p>
+        <p className="flex justify-center">
+          Using&nbsp;
+          <a href="https://github.com/LibreTranslate/LibreTranslate" className="underline">
+            LibreTranslate API
+          </a>
+        </p>
+        <p className="flex justify-center">
+          Hosted by&nbsp;
+          <a href="https://github.com/argosopentech/argos-translate/" className="underline">
+            Argos Translate
+          </a>
+        </p>
+        <p className="flex justify-center">Version: {`${process.env.REACT_APP_VERSION}`}</p>
       </div>
     </div>
   );
